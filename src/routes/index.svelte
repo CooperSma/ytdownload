003d79aa2af57c6da506c6dba456f31a072b92d5
root <you@example.com>
2022-03-08 17:14:44 +0000

    hi

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -70,7 +70,7 @@
 		</form>
 		{#if stage == "download"}
 		<br>
-		<a class = "text-sm" href = "{yturl}" download>Download</a>
+		<a class = "text-sm" href = "{dlurl}" download>Download</a>
 		<br>
 		{/if}
 		<h3 id="error" class="text-red text-sm">{error}</h3>
